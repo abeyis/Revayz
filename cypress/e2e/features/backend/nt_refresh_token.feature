2ac4938a6bdcbada201ffd302522b0c268a1353e
nursena-tekoglu <nursena.tekoglu@abeyis.com>
2023-10-10 21:18:58 +0100

    updated

--- cypress/e2e/features/backend/nt_refresh_token.feature
+++ cypress/e2e/features/backend/nt_refresh_token.feature
@@ -7,14 +7,4 @@ Scenario: User refreshes their access token
     Then a new access token is generated    
     
 
-# Scenario: User tries to refresh with an invalid refresh token
-#     Given an invalid refresh token provided
-#     When the user requests a new access token using the refresh token feature
-#     Then an error response is returned with status code 404
-
-
-# Scenario: User tries to refresh without providing a refresh token
-#     Given no refresh token provided
-#     When the user requests a new access token using the refresh token feature
-#     Then an error response is returned 
 
--- cypress/support/api.commands.js
+++ cypress/support/api.commands.js
@@ -45,6 +45,33 @@ Cypress.Commands.add('userSubscription', () => {
 });
 
 
+//custom command to refresh token -nt
+
+Cypress.Commands.add ('refreshToken', (url, body) => { 
+
+  cy.fixture ('refresh_token2.json').as('refresh_token_data');
+
+  cy.get('@refresh_token_data').then((refresh_token_data) => {
+      var refresh_token2 = refresh_token_data.refresh_token2;
+
+
+      cy.request({
+          method: 'POST',
+          url: Cypress.env('revayz_endpoint') + 'token_generate',
+          body:  {refresh_token2}
+         })   
+         .then ((response) => { 
+          expect(response.body).to.have.property ('idToken');
+          expect(response.status).to.equal (200);
+
+      });
+
+  })
+
+
+})
+
+
 
 Cypress.Commands.add('upGradeDownGradeUsersubscription', (tier_type) => {
   let new_package_id = null;
