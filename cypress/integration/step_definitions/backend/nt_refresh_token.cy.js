b3b088ca679d4588206bc1bd54256061ba2b7194
nursena-tekoglu <nursena.tekoglu@abeyis.com>
2023-10-10 15:39:15 +0100

    step definitions are updated

--- cypress/integration/step_definitions/backend/nt_refresh_token.cy.js
+++ cypress/integration/step_definitions/backend/nt_refresh_token.cy.js
@@ -9,24 +9,36 @@
 import {And, Given, Then, When } from "cypress-cucumber-preprocessor/steps";
 
 
+let refreshTokenResponse; // Define a global variable to hold the response
+
+
 //step sefinitions for Background: The user is logged in
+
 Given ('a valid refresh token is provided', () => {
     
-    cy.refreshToken()
+    cy.refreshToken(). then((response) => { 
+           refreshTokenResponse = response; //store the reponse in the global variable
+        
+      });
 })
 
 
 //Step definitions for Scenario: User refreshes their access token 
+
 Then ('a new access token is generated', () => {
     
-    cy.refreshToken().then((response) => { 
-           
-        expect(response.body).to.have.property ('idToken');
-        expect(response.status).to.equal (200);
+        // Check if refreshTokenResponse exists and has the required properties
 
-      });
+        if (refreshTokenResponse) {
+            expect(refreshTokenResponse.body).to.have.property('idToken');
+            expect(refreshTokenResponse.status).to.equal(200);
+        } else {
+            throw new Error('No valid response found in refreshTokenResponse');
+        }
 
-} )
+    });
+
+
 
 
 
