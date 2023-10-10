986fd636ad9175aa4938c17faac114de60280e46
nursena-tekoglu <nursena.tekoglu@abeyis.com>
2023-10-10 22:03:00 +0100

    successfully completed

--- cypress/support/api.commands.js
+++ cypress/support/api.commands.js
@@ -47,18 +47,18 @@ Cypress.Commands.add('userSubscription', () => {
 
 //custom command to refresh token -nt
 
-Cypress.Commands.add ('refreshToken', (url, body) => { 
+Cypress.Commands.add ('refreshToken', () => { 
 
-  cy.fixture ('refresh_token2.json').as('refresh_token_data');
-
-  cy.get('@refresh_token_data').then((refresh_token_data) => {
-      var refresh_token2 = refresh_token_data.refresh_token2;
-
+  cy.fixture('refresh_token2').then((refresh_token_data) => {
+      var refresh_token2 = refresh_token_data.refresh_token;
+      cy.log(refresh_token2);
 
       cy.request({
           method: 'POST',
           url: Cypress.env('revayz_endpoint') + 'token_generate',
-          body:  {refresh_token2}
+          body:  {
+            refresh_token: refresh_token2
+          }
          })   
          .then ((response) => { 
           expect(response.body).to.have.property ('idToken');
