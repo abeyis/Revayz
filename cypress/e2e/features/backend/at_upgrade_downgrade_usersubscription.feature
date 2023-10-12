4ca39b1597844b81c9eaefd39f4468afe0a8a188
ahmet479777 <ahmet.toprak@abeyis.com>
2023-10-12 14:46:57 +0200

    Finished at_upgrade_downgrade_usersubscription

--- cypress/e2e/features/backend/at_upgrade_downgrade_usersubscription.feature
+++ cypress/e2e/features/backend/at_upgrade_downgrade_usersubscription.feature
@@ -3,11 +3,11 @@ Feature: Upgrade or downgrade usersubscription
 Background: User gets refresh_token
 Given User should get refresh_token create authToken
 
-Scenario Outline: Upgrade or downgrade usersubscription to any tier
+Scenario Outline: Upgrade or downgrade usersubscription to <tier_type>
 When User sends post request to upgrade_downgrade_usersubscription endpoint with "<tier_type>"
-Then user should able to upgrade or downgrade the subscription 
+Then User should able to upgrade or downgrade the subscription 
 Examples: 
     | tier_type| 
     | tier_1   | 
     | tier_2   | 
-    | tier_1   |
+    | tier_free|
--- cypress/integration/step_definitions/backend/at_upgrade_downgrade_usersubscription.cy.js
+++ cypress/integration/step_definitions/backend/at_upgrade_downgrade_usersubscription.cy.js
@@ -1,33 +1,17 @@
 import {When, Then} from "cypress-cucumber-preprocessor/steps";
-
+let tier
 
 Given('User should get refresh_token create authToken', () => {
 cy.generateToken();
 });
 
-When('User sends post request to upgrade_downgrade_usersubscription endpoint',(tier_type) =>{  
-       cy.upGradeDownGradeUsersubscription(tier_type)    
+When('User sends post request to upgrade_downgrade_usersubscription endpoint with {string}',(tier_type) =>{  
+       cy.upGradeDownGradeUsersubscription(tier_type)  
+       tier = tier_type  
 });
-Then('User verify credits with the new plan {string}', (tier_type)=> {
-    cy.wait(1000);
-    cy.userSubscription() .then((Response)=>{
-      expect(Response.status).to.eq(200);
-      const remindRevayzCount= (Response.body.remind_revayz_count); 
-      
-      switch(tier_type){
-          case 'tier_1':
-                expect(remindRevayzCount).to.eq('200');
-                break;
-  
-          case 'tier_2':
-                expect(remindRevayzCount).to.eq('Unlimited');
-                break;
-            
-          default: 
-            throw new Error(`Invalid tier_type: ${tier_type}`);
-  
-      }
-  
-    });
-  
+Then('User should able to upgrade or downgrade the subscription', ()=> {
+ 
+   cy.usersubscription().then((Response)=>{
+    expect(Response.body.package_name).to.eq(cy.findPackageName(tier))
+   })
   });
